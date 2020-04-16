f621c837c60e504e25d6da30524832d8322794c5
TwanS94 <61693492+TwanS94@users.noreply.github.com>
2020-04-16 14:57:17 +0200

    RPS backup

--- Rock, Paper, Scissors.js
+++ Rock, Paper, Scissors.js
@@ -1,4 +1,4 @@
-function RPS(){
+/* function RPS(){
 
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -129,3 +129,4 @@ var cpuWins = 0;
     break;
     }
 }
+*/
